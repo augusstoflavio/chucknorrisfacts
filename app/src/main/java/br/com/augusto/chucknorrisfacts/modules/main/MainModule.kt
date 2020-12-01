b95893e09c435718d8d22f9f55417b69aa87bebc
Augusto Flávio Mendonça <augusto.flaviom@gmail.com>
2020-11-30 21:39:33 -0300

    feat: configura o retrofit para a api chucknorris
    Configura a factory do retrofit, já apontando para a api chucknorris e adiciona no MainModule.kt para a injecao de dependencias

--- app/src/main/java/br/com/augusto/chucknorrisfacts/modules/main/MainModule.kt
+++ app/src/main/java/br/com/augusto/chucknorrisfacts/modules/main/MainModule.kt
@@ -1,12 +1,18 @@
 package br.com.augusto.chucknorrisfacts.modules.main
 
 import android.content.Context
+import br.com.augusto.chucknorrisfacts.app.api.RetrofitFactory
 import br.com.augusto.chucknorrisfacts.app.module.ModuleInterface
 import org.koin.core.module.Module
+import org.koin.dsl.module
 
 class MainModule: ModuleInterface {
 
     override fun getKoinModule(context: Context): Module? {
-        return null
+        return module {
+            factory {
+                RetrofitFactory.getInstance()
+            }
+        }
     }
 }
