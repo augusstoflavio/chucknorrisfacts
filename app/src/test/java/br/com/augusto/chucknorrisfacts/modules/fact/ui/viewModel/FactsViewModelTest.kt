b8af1dd8448731545a14b662b3c04c690bacd3e1
Augusto Flávio Mendonça <augusto.flaviom@gmail.com>
2020-12-08 22:19:08 -0300

    test: testa a verificacao de conexao na consulta dos fatos

---
+++ app/src/test/java/br/com/augusto/chucknorrisfacts/modules/fact/ui/viewModel/FactsViewModelTest.kt
@@ -0,0 +1,63 @@
+package br.com.augusto.chucknorrisfacts.modules.fact.ui.viewModel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import br.com.augusto.chucknorrisfacts.app.commons.INetworkState
+import br.com.augusto.chucknorrisfacts.modules.fact.data.repository.IFactRepository
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class FactsViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var viewModel: FactsViewModel
+
+    @Mock
+    private lateinit var factRepository: IFactRepository
+
+    @Mock
+    private lateinit var errorObserver: Observer<String?>
+
+    @Captor
+    private lateinit var argumentCaptor: ArgumentCaptor<String?>
+
+    @Test
+    fun `when offline then set error`() {
+        viewModel = FactsViewModel(factRepository, OfflineNetworkState())
+        viewModel.error.observeForever(errorObserver)
+
+        viewModel.searchFacts("word")
+
+        Mockito.verify(errorObserver, Mockito.times(1))
+            .onChanged(argumentCaptor.capture())
+
+        val values = argumentCaptor.allValues
+        Assert.assertFalse(values.isEmpty())
+        Assert.assertEquals("Verifique a sua conexão", values[0])
+    }
+
+    @Before
+    fun setupMyTripViewModel() {
+        RxJavaPlugins.setIoSchedulerHandler{ Schedulers.trampoline()}
+    }
+}
+
+class OfflineNetworkState: INetworkState {
+
+    override fun isConnected(): Boolean {
+        return false
+    }
+}
