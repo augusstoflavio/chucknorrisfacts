26a19f1f55fea9f479f63c3a15ccd8c7e025820d
Augusto Flávio <augusto.flavio@work.co>
2023-05-20 16:32:20 -0300

    Reformat: Remove unnecessary methods from the Fact model

--- app/src/main/java/br/com/augusto/chucknorrisfacts/domain/model/Fact.kt
+++ app/src/main/java/br/com/augusto/chucknorrisfacts/domain/model/Fact.kt
@@ -3,17 +3,5 @@ package br.com.augusto.chucknorrisfacts.domain.model
 data class Fact(
     var categories: List<String>,
     var value: String,
-    var url: String
-) {
-    fun isLongFact(): Boolean {
-        return value.length > 80
-    }
-
-    fun getCategory(): String {
-        if (categories.isEmpty()) {
-            return "Uncategorized"
-        }
-
-        return categories.first()
-    }
-}
+    var url: String,
+)
