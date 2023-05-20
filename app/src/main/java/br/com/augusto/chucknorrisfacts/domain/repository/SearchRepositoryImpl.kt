bba590d382cbb9656b7addcb3131744cb98b83d9
Augusto Flávio <augusto.flavio@work.co>
2023-05-20 15:11:28 -0300

    Style: Reformat code

--- app/src/main/java/br/com/augusto/chucknorrisfacts/domain/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/br/com/augusto/chucknorrisfacts/domain/repository/SearchRepositoryImpl.kt
@@ -1,10 +1,8 @@
 package br.com.augusto.chucknorrisfacts.domain.repository
 
-import br.com.augusto.chucknorrisfacts.data.local.entity.SearchEntity
 import br.com.augusto.chucknorrisfacts.domain.Result
 import br.com.augusto.chucknorrisfacts.domain.dataSource.SearchDataSource
 import br.com.augusto.chucknorrisfacts.domain.model.Search
-import java.time.LocalDateTime
 
 class SearchRepositoryImpl(
     private val searchDataSource: SearchDataSource,
@@ -16,7 +14,7 @@ class SearchRepositoryImpl(
 
     override suspend fun saveSearch(search: Search): Result<Unit> {
         return searchDataSource.saveSearch(
-            search
+            search,
         )
     }
 }
