6814091bc2eaee2d2d28da6cb48a8bb06f702c4f
Augusto Flávio <augusto.flavio@work.co>
2023-05-20 16:22:36 -0300

    Reformat: Create specific error types

--- app/src/main/java/br/com/augusto/chucknorrisfacts/domain/Result.kt
+++ app/src/main/java/br/com/augusto/chucknorrisfacts/domain/Result.kt
@@ -3,5 +3,12 @@ package br.com.augusto.chucknorrisfacts.domain
 sealed interface Result<out T> {
 
     data class Success<out T>(val data: T) : Result<T>
-    data class Error(val data: Exception) : Result<Nothing>
+    sealed interface Error : Result<Nothing> {
+
+        object ApiError : Error
+
+        object NoConnectionError : Error
+
+        data class InternalError(val exception: Exception) : Error
+    }
 }
