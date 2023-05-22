88877633080613607ed317e23c6c35179dad1605
Augusto Flávio <augusto.flavio@work.co>
2023-05-22 20:23:57 -0300

    Reformat: Gets categories only locally on search facts fragment

--- app/src/main/java/br/com/augusto/chucknorrisfacts/domain/repository/CategoryRepository.kt
+++ app/src/main/java/br/com/augusto/chucknorrisfacts/domain/repository/CategoryRepository.kt
@@ -2,8 +2,7 @@ package br.com.augusto.chucknorrisfacts.domain.repository
 
 import br.com.augusto.chucknorrisfacts.domain.Result
 import br.com.augusto.chucknorrisfacts.domain.model.Category
-import kotlinx.coroutines.flow.Flow
 
 interface CategoryRepository {
-    suspend fun getCategories(): Flow<Result<List<Category>>>
+    suspend fun getCategories(amount: Int): Result<List<Category>>
 }
