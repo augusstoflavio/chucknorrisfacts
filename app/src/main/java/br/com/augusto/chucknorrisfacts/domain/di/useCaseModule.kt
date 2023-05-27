a5210a860e3f99a83fcd8d49769f78652da1743c
Augusto Flávio <augusto.flavio@work.co>
2023-05-27 11:13:39 -0300

    Fix: Fix creation of SyncCategoriesUseCaseImpl on dependency injection

--- app/src/main/java/br/com/augusto/chucknorrisfacts/domain/di/useCaseModule.kt
+++ app/src/main/java/br/com/augusto/chucknorrisfacts/domain/di/useCaseModule.kt
@@ -1,9 +1,6 @@
 package br.com.augusto.chucknorrisfacts.domain.di
 
-import br.com.augusto.chucknorrisfacts.domain.useCase.GetRandomCategoriesUseCase
-import br.com.augusto.chucknorrisfacts.domain.useCase.GetLatestSearchesUseCase
-import br.com.augusto.chucknorrisfacts.domain.useCase.SearchFactsUseCase
-import br.com.augusto.chucknorrisfacts.domain.useCase.SyncCategoriesUseCaseImpl
+import br.com.augusto.chucknorrisfacts.domain.useCase.*
 import org.koin.dsl.module
 
 val useCaseModule = module {
@@ -19,7 +16,7 @@ val useCaseModule = module {
         SearchFactsUseCase(get(), get())
     }
 
-    factory {
+    factory<SyncCategoriesUseCase> {
         SyncCategoriesUseCaseImpl(get())
     }
 }
